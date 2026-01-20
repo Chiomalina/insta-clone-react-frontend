0c1032094d72538a5fb7eda4e61cc312f2d8f52d
iamchioma <chiomalinaanaso@gmail.com>
2026-01-20 03:34:41 +0100

    style PostCard components

--- app/components/PostCard.tsx
+++ app/components/PostCard.tsx
@@ -17,21 +17,31 @@ export function PostCard({
       ? post.img_url
       : `${API_BASE_URL}${post.img_url}`;
   return (
-    <div className="w-full max-w-lg mx-auto rounded-lg overflow-hidden border bg-white mb-6">
-      <div className="p-4">
-        <p className="font-bold">{username}</p>
+    <div className="w-full max-w-lg mx-auto mb-6 rounded-xl border bg-white shadow-sm hover:shadow-md transition-shadow duration-200 ">
+      {/* Header  */}
+      <div className="flex items-center gap-3 px-4 opacity-3" >
+        <div className="h-9 w-9 rounded-full bg-gray-200 flex items-center justify-center text-sm font-semibold">
+          {username[0].toUpperCase()}
+        </div>
+        <p className="font-semibold text-sm">{username}</p>
       </div>
 
-      <img
-        src={imageSrc}
-        alt={post.caption || "Instagram post"}
-        className="w-full h-auto aspect-square object-cover"
-      />
+      { /*Image  */}
+      <div className="w-full aspect-square bg-gray-100">
+        <img
+          src={imageSrc}
+          alt={post.caption || "Instagram post"}
+          className="h-full w-full object-cover"
+        />
+      </div>
 
-      <div className="p-4">
+      { /* Caption */}
+      <div className="p-4 py-3 text-sm">
         <p>
-          <span className="font-bold mr-2">{username}</span>
-          {post.caption}
+          <span className="font-semibold mr-2">{username}</span>
+          <span className="text-gray-700">
+            {post.caption || "No caption provided."}
+          </span>
         </p>
       </div>
     </div>
